871b27adde5eb4263fb0541a7a9f6747bf02cfed
eliasciur <afakedemail@yahoo.com>
2023-11-24 09:07:03 -0600

    fixed coinflip images

--- src/commands/coinflip.ts
+++ src/commands/coinflip.ts
@@ -10,10 +10,8 @@ module.exports = {
 
       const side = Math.round(Math.random());
        
-      interaction.reply({embeds: [new Embed({color: 0x22b1fc, title: "Coin Flip",
-         description: `The coin is ${side == 0? "heads" : "tails"}!`}).setImage(side == 0? 
-            "https://github.com/eliasciur/elias-bot/blob/main/src/img/heads.png"
-            : "https://github.com/eliasciur/elias-bot/blob/main/src/img/tails.png")]});
+      interaction.reply({embeds: [new Embed({color: 0x22b1fc, title: "Coin Flip", description: `The coin is ${side == 0? "heads" : "tails"}!`})
+         .setImage(side == 0? "https://i.imgur.com/WwIZMNe.png" : "https://i.imgur.com/3Pvhkka.png")]});
 
    },
 
