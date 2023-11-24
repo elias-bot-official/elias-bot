0f5847407a60c02c1e91acac03494802e557f138
eliasciur <afakedemail@yahoo.com>
2023-11-24 09:00:26 -0600

    added image to coinflip embed

--- src/commands/coinflip.ts
+++ src/commands/coinflip.ts
@@ -7,9 +7,13 @@ module.exports = {
    data: new SlashCommandBuilder().setName("coin-flip").setDescription("Flips a coin."),
    
    onCommandInteraction(interaction) {
+
+      const side = Math.round(Math.random());
        
       interaction.reply({embeds: [new Embed({color: 0x22b1fc, title: "Coin Flip",
-         description: `The coin is ${Math.round(Math.random()) == 0? "heads" : "tails"}!`})]});
+         description: `The coin is ${side == 0? "heads" : "tails"}!`}).setImage(side == 0? 
+            "https://github.com/eliasciur/elias-bot/blob/main/src/img/heads.png"
+            : "https://github.com/eliasciur/elias-bot/blob/main/src/img/tails.png")]});
 
    },
 
