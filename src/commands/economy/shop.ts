cf1ee87d7c0283ff4ad40ef952078e2aec4c00c3
eliasciur <afakedemail@yahoo.com>
2023-11-28 19:04:03 -0600

    added shop command

---
+++ src/commands/economy/shop.ts
@@ -0,0 +1,60 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandIntegerOption, SlashCommandStringOption, SlashCommandSubcommandBuilder } from "discord.js";
+import { Command } from "../../structure/Command";
+import shop from "../../json/shop.json";
+import { Embed } from "../../structure/Embed";
+import { User } from "../../schemas/User";
+import emojis from "../../json/emojis.json";
+
+module.exports = {
+
+   data: new SlashCommandBuilder().setName("shop").setDescription("Commands related to the shop.")
+      .addSubcommand(new SlashCommandSubcommandBuilder().setName("view").setDescription("View the items in the shop."))
+      .addSubcommand(new SlashCommandSubcommandBuilder().setName("buy").setDescription("Buys an item from the shop.")
+         .addStringOption(new SlashCommandStringOption().setName("item").setDescription("The name of the item you want to buy.")
+            .addChoices({name: 'lock', value: 'lock'}, {name: 'lockpick', value: 'lockpick'}).setRequired(true))
+         .addIntegerOption(new SlashCommandIntegerOption().setName("amount").setDescription("The amount you want to buy.").setMinValue(1))),
+
+   async onCommandInteraction(interaction: ChatInputCommandInteraction) {
+       
+      switch (interaction.options.getSubcommand()) {
+
+         case 'view': {
+
+            let description = '';
+            shop.forEach(item => description += `${emojis[item.name]} ${item.name}: ${item.price.toLocaleString()} ${emojis.coin}\n`);
+
+            interaction.reply({embeds: [new Embed({color: 0x22b1fc, title: "Items", description: description})]});
+            return;
+
+         }
+
+         case 'buy': {
+
+            const itemName = interaction.options.getString("item");
+            const item = shop.filter(item => item.name == itemName)[0];
+            const amount = interaction.options.getInteger("amount", false) ?? 1;
+            const user = await User.findById(interaction.user.id);
+
+            if (!user || item.price * amount > user.balance) {
+
+               interaction.reply({embeds: [new Embed({color: 0xED4245, title: 'Error',
+                  description: 'You do not have enough money!'})], ephemeral: true});
+               return;
+
+            }
+
+            interaction.reply({embeds: [new Embed({color: 0x22b1fc, title: "Buy",
+               description: `You bought **${amount}x** ${itemName}!`})]});
+            
+            user.inventory[itemName] = (user.inventory[itemName] ?? 0) + amount;
+            user.balance -= item.price * amount;
+            user.save();
+            return;
+
+         }
+
+      }
+
+   },
+
+} satisfies Command
