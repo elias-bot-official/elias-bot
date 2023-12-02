a42cacf7134399ef5f76a4ac71596f16230934cf
eliasciur <afakedemail@yahoo.com>
2023-12-02 14:43:01 -0600

    updated Button class

--- src/structure/Button.ts
+++ src/structure/Button.ts
@@ -1,20 +1,47 @@
-import { ButtonBuilder, ButtonStyle } from "discord.js";
+import { ButtonBuilder, ButtonStyle, ComponentEmojiResolvable } from "discord.js";
+
+interface BaseButton {
+
+   label?: string;
+   emoji?: ComponentEmojiResolvable;
+   disabled?: true;
+
+}
 
 export class Button extends ButtonBuilder {
 
-   constructor({id, label, emoji, style, url: url, disabled}: {id?: string, label?: string, emoji?: string, style: ButtonStyle, url?: string, disabled?: true}) {
+   static primary(props: BaseButton & {custom_id: string}) {
 
-      super();
-      if (id)
-         this.setCustomId(id);
-      if (label)
-         this.setLabel(label);
-      if (emoji)
-         this.setEmoji(emoji);
-      this.setStyle(style);
-      if (url)
-         this.setURL(url);
-      this.setDisabled(disabled ?? false);
+      return new ButtonBuilder({ style: ButtonStyle.Primary, custom_id: props.custom_id,
+         label: props.label, emoji: props.emoji, disabled: props.disabled ?? false });
+
+   }
+
+   static secondary(props: BaseButton & {custom_id: string}) {
+
+      return new ButtonBuilder({ style: ButtonStyle.Secondary, custom_id: props.custom_id,
+         label: props.label, emoji: props.emoji, disabled: props.disabled ?? false });
+
+   }
+
+   static success(props: BaseButton & {custom_id: string}) {
+
+      return new ButtonBuilder({ style: ButtonStyle.Success, custom_id: props.custom_id,
+         label: props.label, emoji: props.emoji, disabled: props.disabled ?? false });
+
+   }
+
+   static danger(props: BaseButton & {custom_id: string}) {
+
+      return new ButtonBuilder({ style: ButtonStyle.Danger, custom_id: props.custom_id,
+         label: props.label, emoji: props.emoji, disabled: props.disabled ?? false });
+
+   }
+
+   static link(props: BaseButton & {url: string}) {
+
+      return new ButtonBuilder( {style: ButtonStyle.Link, label: props.label,
+         emoji: props.emoji, url: props.url });
 
    }
 
