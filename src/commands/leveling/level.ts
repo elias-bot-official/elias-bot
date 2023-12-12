d24963eb427ecc0ec790c5c6ac5f9c7cb168519c
eliasciur <afakedemail@yahoo.com>
2023-12-11 21:25:47 -0600

    improved level card

--- src/commands/leveling/level.ts
+++ src/commands/leveling/level.ts
@@ -31,11 +31,30 @@ module.exports = {
 			});
 			return;
 		}
+
+		if (!guild.xp.get(user.id)) {
+			interaction.reply({
+				embeds: [
+					new Embed({
+						color: EmbedColor.danger,
+						title: 'Error',
+						description: 'This user has no XP.'
+					})
+				],
+				ephemeral: true
+			});
+			return;
+		}
+
+		let rank = 1;
+		guild.xp.forEach(xp => {
+			if ((xp > guild.xp.get(user.id)))	rank++;
+		});
 		
 		interaction.guild.members
 			.fetch(user.id)
 			.then(async member => {
-				const card = await drawCard(guild, member);
+				const card = await drawCard(member, guild.xp.get(user.id) as number, rank);
 
 				interaction.reply({
 					files: [
@@ -51,16 +70,17 @@ module.exports = {
 							title: 'Error',
 							description: 'Can not find this user in this server.',
 						}),
-					]
+					],
+					ephemeral: true
 				});
 			});
 	},
 } satisfies Command;
 
-async function drawCard(guild, member: GuildMember) {
-	const xp = guild.xp.get(member.id) as number ?? 0;
+async function drawCard(member: GuildMember, xp: number, rank: number) {
 	const level = getLevel(xp);
 	const neededXP = getXP(level + 1);
+	const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
 	const canvas = createCanvas(550, 150);
 	const ctx = canvas.getContext('2d');
@@ -68,35 +88,28 @@ async function drawCard(guild, member: GuildMember) {
 
 	// background
 	ctx.beginPath();
-	ctx.fillStyle = '#101214';
+	ctx.fillStyle = '#2b2d30';
 	ctx.fillRect(0, 0, 550, 150);
 	ctx.closePath();
 
-	// name
+	// avatar
 	ctx.beginPath();
-	ctx.fillStyle = '#ffff';
-	ctx.textBaseline = 'bottom';
-	ctx.font = '35px Arial';
-	ctx.fillText(member.displayName, 135, 85);
+	ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
 	ctx.closePath();
-
-	// level
-	ctx.beginPath();
-	ctx.fillStyle = '#ffff';
-	ctx.font = '25px Arial';
-	ctx.textAlign = 'end';
-	ctx.fillText(`Level ${level}`, 520, 82);
-	ctx.closePath();
+	ctx.save();
+	ctx.clip();
+	ctx.drawImage(avatar, 25, 25, 100, 100);
+	ctx.restore();
 
 	// white part of progress bar
 	ctx.beginPath();
 	ctx.fillStyle = '#ffff';
 	ctx.roundRect(135, 90, 385, 20, 10);
 	ctx.fill();
+	ctx.closePath();
 	ctx.save();
 	ctx.clip();
-	ctx.closePath();
-
+	
 	// colored part of progress bar
 	ctx.beginPath();
 	ctx.fillStyle = '#04a0fb';
@@ -105,12 +118,31 @@ async function drawCard(guild, member: GuildMember) {
 	ctx.closePath();
 	ctx.restore();
 
-	// avatar
+	// name
 	ctx.beginPath();
 	ctx.fillStyle = '#ffff';
-	ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
-	ctx.clip();
-	ctx.drawImage(avatar, 25, 25, 100, 100);
+	ctx.textBaseline = 'bottom';
+	ctx.font = '30px roboto';
+	ctx.fillText(member.displayName, 135, 85);
+	ctx.closePath();
+
+	// xp
+	ctx.beginPath();
+	ctx.textAlign = 'end';
+	ctx.font = '16px roboto';
+	ctx.fillText(`${formatter.format(xp - getXP(level))} / ${formatter.format(neededXP - getXP(level))} XP`, 520, 83);
+	ctx.closePath();
+
+	// rank
+	ctx.beginPath();
+	ctx.font = '22px roboto';
+	ctx.textBaseline = 'top';
+	ctx.fillText(`RANK ${rank}`, 395, 20);
+	ctx.closePath();
+
+	// level
+	ctx.beginPath();
+	ctx.fillText(`LEVEL ${level}`, 520, 20);
 	ctx.closePath();
 
 	return canvas.toBuffer();
