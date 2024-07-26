69d38fedb259d0655a2289ec5cb0dd8ba67c67f4
eliasciur <afakedemail@yahoo.com>
2024-07-25 21:35:17 -0500

    fixed formatting

--- src/commands/global/toggle.ts
+++ src/commands/global/toggle.ts
@@ -7,80 +7,80 @@ import fs from 'fs';
 import path from 'path';
 
 module.exports = {
-    data: new SlashCommandBuilder()
+	data: new SlashCommandBuilder()
 		.setName('toggle')
-        .setDescription('Toggles a plugin.')
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .setDMPermission(false)
-        .addStringOption(
-            new SlashCommandStringOption()
-                .setName('plugin')
-                .setDescription('The plugin you want to toggle.')
-                .addChoices(
-                    { name: 'Economy', value: 'Economy' },
-                    { name: 'Fun', value: 'Fun' },
-                    { name: 'Leveling', value: 'Leveling' },
-                    { name: 'Moderation', value: 'Moderation' },
-                    { name: 'Saluter', value: 'Saluter' }
-                )
-                .setRequired(true)
-        ),
+		.setDescription('Toggles a plugin.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.setDMPermission(false)
+		.addStringOption(
+			new SlashCommandStringOption()
+				.setName('plugin')
+				.setDescription('The plugin you want to toggle.')
+				.addChoices(
+					{ name: 'Economy', value: 'Economy' },
+					{ name: 'Fun', value: 'Fun' },
+					{ name: 'Leveling', value: 'Leveling' },
+					{ name: 'Moderation', value: 'Moderation' },
+					{ name: 'Saluter', value: 'Saluter' }
+				)
+				.setRequired(true)
+		),
 
-    async onCommandInteraction(interaction: ChatInputCommandInteraction) {
-        await interaction.deferReply();
+	async onCommandInteraction(interaction: ChatInputCommandInteraction) {
+		await interaction.deferReply();
 
-        const pluginName = interaction.options.getString('plugin');
-        const guild = await GuildModel.findById(interaction.guild.id) ?? await GuildModel.create({ _id: interaction.guild.id });
+		const pluginName = interaction.options.getString('plugin');
+		const guild = await GuildModel.findById(interaction.guild.id) ?? await GuildModel.create({ _id: interaction.guild.id });
 
-        if (guild.plugins.includes(pluginName)) {
-            if (fs.existsSync(path.join(__dirname, '..', pluginName))) {
-                const commandNames = [];
+		if (guild.plugins.includes(pluginName)) {
+			if (fs.existsSync(path.join(__dirname, '..', pluginName))) {
+				const commandNames = [];
 
-                fs.readdirSync(path.join(__dirname, '..', pluginName)).forEach(file => {
-                    commandNames.push(file.split('.')[0]);
-                });
+				fs.readdirSync(path.join(__dirname, '..', pluginName)).forEach(file => {
+					commandNames.push(file.split('.')[0]);
+				});
 
-                const guildCommands = await interaction.guild.commands.fetch();
+				const guildCommands = await interaction.guild.commands.fetch();
 
-                for (const entry of guildCommands) {
-                    if (entry[1].client.application.id == interaction.client.application.id &&
-                        commandNames.includes(entry[1].name)) {
-                        interaction.guild.commands.delete(entry[1].id);
-                    }
-                }
-            }
+				for (const entry of guildCommands) {
+					if (entry[1].client.application.id == interaction.client.application.id &&
+							commandNames.includes(entry[1].name)) {
+						interaction.guild.commands.delete(entry[1].id);
+					}
+				}
+			}
 
-            guild.plugins.splice(guild.plugins.indexOf(pluginName), 1);
+			guild.plugins.splice(guild.plugins.indexOf(pluginName), 1);
 
-            await interaction.editReply({
-                embeds: [
-                    new Embed({
-                        color: EmbedColor.primary,
-                        description: `The \`${pluginName}\` plugin has been disabled.`
-                    })
-                ]
-            });
-        }
-        else {
-            if (fs.existsSync(path.join(__dirname, '..', pluginName))) {
-                fs.readdirSync(path.join(__dirname, '..', pluginName)).forEach(file => {
-                    const command = require(path.join(__dirname, '..', pluginName, file));
-                    interaction.guild.commands.create(command.data);
-                });
-            }
+			await interaction.editReply({
+				embeds: [
+					new Embed({
+						color: EmbedColor.primary,
+						description: `The \`${pluginName}\` plugin has been disabled.`
+					})
+				]
+			});
+		}
+		else {
+			if (fs.existsSync(path.join(__dirname, '..', pluginName))) {
+				fs.readdirSync(path.join(__dirname, '..', pluginName)).forEach(file => {
+					const command = require(path.join(__dirname, '..', pluginName, file));
+					interaction.guild.commands.create(command.data);
+				});
+			}
 
-            guild.plugins.push(pluginName);
+			guild.plugins.push(pluginName);
 
-            await interaction.editReply({
-                embeds: [
-                    new Embed({
-                        color: EmbedColor.primary,
-                        description: `The \`${pluginName}\` plugin has been enabled.`
-                    })
-                ]
-            });
-        }
+			await interaction.editReply({
+				embeds: [
+					new Embed({
+						color: EmbedColor.primary,
+						description: `The \`${pluginName}\` plugin has been enabled.`
+					})
+				]
+			});
+		}
 
-        await guild.save();
-    }
+		await guild.save();
+	}
 } satisfies Command;
--- src/events/guildAvailable.ts
+++ src/events/guildAvailable.ts
@@ -6,26 +6,26 @@ import path from 'path';
 import fs from 'node:fs';
 
 module.exports = {
-    async execute(guild: Guild) {
-        if (process.env.NODE_ENV != 'production') return;
+	async execute(guild: Guild) {
+		if (process.env.NODE_ENV != 'production') return;
 
-        guild.client.rest.put(
-            Routes.applicationGuildCommands(process.env.id, guild.id),
-            { body: [] }
-        );
+		guild.client.rest.put(
+			Routes.applicationGuildCommands(process.env.id, guild.id),
+			{ body: [] }
+		);
 
-        const dbGuild = await GuildModel.findById(guild.id);
-        if (!dbGuild) return;
+		const dbGuild = await GuildModel.findById(guild.id);
+		if (!dbGuild) return;
 
-        dbGuild.plugins.forEach(name => {
-            const pluginPath = path.join(__dirname, '..', 'commands', name);
+		dbGuild.plugins.forEach(name => {
+			const pluginPath = path.join(__dirname, '..', 'commands', name);
             
-            if (!fs.existsSync(pluginPath)) return;
+			if (!fs.existsSync(pluginPath)) return;
 
-            fs.readdirSync(pluginPath).forEach(file => {
-                const command = require(path.join(pluginPath, file));
-                guild.commands.create(command.data);
-            });
-        });
-    }
+			fs.readdirSync(pluginPath).forEach(file => {
+				const command = require(path.join(pluginPath, file));
+				guild.commands.create(command.data);
+			});
+		});
+	}
 } satisfies DiscordEvent;
