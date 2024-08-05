55f1bb5bc1ea7208b1c575c49c4829fe3dd0a6bc
Elias <79549857+eliasciur@users.noreply.github.com>
2024-08-05 17:23:17 -0500

    Update afk.ts

--- src/commands/Fun/afk.ts
+++ src/commands/Fun/afk.ts
@@ -1,12 +1,13 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, SlashCommandBooleanOption, SlashCommandChannelOption, TextChannel, GuildMember } from 'discord.js';
 import { Command } from '../../structure/Command';
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('afk')
 		.setDescription('Marks you as AFK in a specific channel or whole server.')
 		.addBooleanOption(
 			new SlashCommandBooleanOption()
-				.setName('all_channels')
+				.setName('server')
 				.setDescription('Set AFK status for the whole server.')
 				.setRequired(true)
 		)
@@ -14,18 +15,16 @@ module.exports = {
 			new SlashCommandChannelOption()
 				.setName('channel')
 				.setDescription('The specific channel to set AFK status for.')
-				.setRequired(false)
 		),
 
 	async onCommandInteraction(interaction: ChatInputCommandInteraction) {
-		const allChannels = interaction.options.getBoolean('all_channels');
+		const allChannels = interaction.options.getBoolean('server');
 		const channel = interaction.options.getChannel('channel') as TextChannel | null;
 		const member = interaction.member as GuildMember;
 		const afkRoleId = '1270108233234776137';
 
 		if (allChannels) {
 			try {
-				// Add AFK role to the user
 				await member.roles.add(afkRoleId);
 				member.user.afk_status = 'AFK for the whole server';
 				await interaction.reply({ content: 'You are now marked as AFK for the whole server.', ephemeral: true });
@@ -37,7 +36,6 @@ module.exports = {
 		}
 		else if (channel) {
 			try {
-				// Add AFK role to the user
 				await member.roles.add(afkRoleId);
 				member.user.afk_status = `AFK in channel ${channel.id}`;
 				await interaction.reply({ content: `You are now marked as AFK for the channel ${channel.name}.`, ephemeral: true });
