f20cb250c7034da82d2858451e33d09c9ccf5a45
Umar Khorami <umar.khorami@gmail.com>
2024-08-05 18:14:40 -0400

    afk.ts + index.ts

--- src/commands/Fun/afk.ts
+++ src/commands/Fun/afk.ts
@@ -1,6 +1,5 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, SlashCommandBooleanOption, SlashCommandChannelOption, TextChannel, GuildMember, PermissionsBitField } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, SlashCommandBooleanOption, SlashCommandChannelOption, TextChannel, GuildMember } from 'discord.js';
 import { Command } from '../../structure/Command';
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('afk')
@@ -22,28 +21,31 @@ module.exports = {
 		const allChannels = interaction.options.getBoolean('all_channels');
 		const channel = interaction.options.getChannel('channel') as TextChannel | null;
 		const member = interaction.member as GuildMember;
+		const afkRoleId = '1270108233234776137';
 
 		if (allChannels) {
-			// check if the bot has the required permissions
-			if (!interaction.guild?.members.me?.permissions.has(PermissionsBitField.Flags.ManageNicknames)) {
-				await interaction.reply({ content: 'I do not have permission to change nicknames. Please give me the `ManageNicknames` permission.', ephemeral: true });
-				return;
-			}
-
 			try {
-				// set AFK status for the whole server
-				await member.setNickname(`[AFK] ${member.displayName}`);
+				// Add AFK role to the user
+				await member.roles.add(afkRoleId);
+				member.user.afk_status = 'AFK for the whole server';
 				await interaction.reply({ content: 'You are now marked as AFK for the whole server.', ephemeral: true });
 			}
-			catch (error) { 
+			catch (error) {
 				console.error(error);
-				await interaction.reply({ content: 'Failed to set AFK status. Please ensure my role is higher than your role.', ephemeral: true });
+				await interaction.reply({ content: 'Failed to set AFK status.', ephemeral: true });
 			}
 		}
 		else if (channel) {
-			// set AFK status for a specific channel
-			// somehow add mongoDB to store AFK status for each channel
-			await interaction.reply({ content: `You are now marked as AFK for the channel ${channel.name}.`, ephemeral: true });
+			try {
+				// Add AFK role to the user
+				await member.roles.add(afkRoleId);
+				member.user.afk_status = `AFK in channel ${channel.id}`;
+				await interaction.reply({ content: `You are now marked as AFK for the channel ${channel.name}.`, ephemeral: true });
+			}
+			catch (error) {
+				console.error(error);
+				await interaction.reply({ content: 'Failed to set AFK status.', ephemeral: true });
+			}
 		}
 		else {
 			await interaction.reply({ content: 'Please specify a channel or set AFK status for the whole server.', ephemeral: true });
