ff5668558d4d0186b5dfe4520cc4433d0982386a
eliasciur <afakedemail@yahoo.com>
2024-11-26 21:24:40 -0600

    renamed `/gamble` to `/roulette`

---
+++ src/commands/economy/roulette.ts
@@ -0,0 +1,78 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandIntegerOption, SlashCommandStringOption } from 'discord.js';
+import { Command } from '../../structure/Command';
+import { Embed, EmbedColor } from '../../structure/Embed';
+import emojis from '../../json/emojis.json';
+import { UserModel } from '../../schemas/User';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roulette')
+		.setDescription('Play a game of roulette.')
+		.addIntegerOption(
+			new SlashCommandIntegerOption()
+				.setName('amount')
+				.setDescription('The amount you want to bet.')
+				.setMinValue(1)
+				.setMaxValue(50_000)
+				.setRequired(true)
+		)
+		.addStringOption(
+			new SlashCommandStringOption()
+				.setName('color')
+				.setDescription('The color you want to bet on.')
+				.setChoices(
+					{ name: 'black', value: 'black' },
+					{ name: 'red', value: 'red' }
+				)
+				.setRequired(true)
+		),
+
+	async onCommandInteraction(interaction: ChatInputCommandInteraction) {
+		const user = await UserModel.findById(interaction.user.id);
+		const amount = interaction.options.getInteger('amount');
+
+		if (!user || user.balance < amount) {
+			interaction.reply({
+				embeds: [
+					new Embed({
+						color: EmbedColor.danger,
+						description: 'You cannot bet more money than you own.'
+					})
+				],
+				ephemeral: true
+			});
+			return;
+		}
+
+		if (Math.round(Math.random()) == 0) {
+			interaction.reply({
+				embeds: [
+					new Embed({
+						color: EmbedColor.primary,
+						title: 'Roulette',
+						description: `You won ${amount.toLocaleString()} ${emojis.coin}!`
+					}).setThumbnail(
+						'https://cdn-icons-png.flaticon.com/512/3425/3425938.png'
+					)
+				]
+			});
+			user.balance += amount;
+		}
+		else {
+			interaction.reply({
+				embeds: [
+					new Embed({
+						color: EmbedColor.primary,
+						title: 'Roulette',
+						description: `You lost ${amount.toLocaleString()} ${emojis.coin}!`
+					}).setThumbnail(
+						'https://cdn-icons-png.flaticon.com/512/3425/3425938.png'
+					)
+				]
+			});
+			user.balance -= amount;
+		}
+
+		user.save();
+	}
+} satisfies Command;
